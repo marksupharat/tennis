911f7aa1d61d54bd339929428a93095d722207bd
marksupharat <mark.supharat@gmail.com>
2016-09-28 21:40:00 +0700

    add scoreA 0-0

--- spec/tennis.spec.js
+++ spec/tennis.spec.js
@@ -1,15 +1,31 @@
-function Tennis(){
-this.start =function(){
-};
-this.echo = function(){
-return 'Love - Love';
-}
+function tennispoint()
+{
+  this.playAGetscore = '';
+  this.playBGetscore = '';
+  this.echo = function()
+  {
+    var str = '';
+
+    if(this.playAGetscore === 0)
+    {
+        str += 'Love-';
+    }
+    if(this.playBGetscore === 0)
+    {
+         str += 'Love';
+    }
+
+      return str;
+  };
 }
 
-describe('Tennis game',function(){
-it('shout echo "Love - Love" when strarting the game',function(){
-var tennis = new Tennis();
-tennis.start();
-expect(tennis.echo()).toEqual('Love-Love');
+  describe('Tennis game', function(){
+	  it('should  be  "Love = Love" when starting the game',function()
+	  {
+       var point = new tennispoint();
+	      point.playAGetscore = 0;
+	      point.playBGetscore = 0;
+	      expect(point.echo()).toEqual('Love-Love');
+	  });
+
 });
-})
