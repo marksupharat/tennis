b4b280c158ca807d624d304c447603b4072695ee
marksupharat <mark.supharat@gmail.com>
2016-09-29 09:49:57 +0700

    edit score case A

--- spec/tennis.spec.js
+++ spec/tennis.spec.js
@@ -1,78 +1,95 @@
-function tennispoint()
-{
-  this.playAGetscore = '';
-  this.playBGetscore = '';
-  this.echo = function()
-  {
-    var str = '';
-
-   if(this.playAGetscore === 0)
-    {
-        str += 'Love-';
-    }
-    else if(this.playAGetscore === 15)
-    {
-        str += 'fifteen-';
-    }
-    else if(this.playAGetscore === 30)
-    {
-        str += 'thirty-';
-    }
-    else if(this.playAGetscore === 40)
-    {
-        str += 'Forty-';
-    }
-
-    if(this.playBGetscore === 0)
-     {
-        str += 'Love';
-     }
-    else if(this.playBGetscore === 15)
-    {
-        str += 'fifteen';
-    }
-    else if(this.playBGetscore === 30)
-    {
-        str += 'thirty' ;
-    }
-    else if(this.playBGetscore === 40)
-    {
-        str += 'Forty' ;
-    }
-
-      return str;
-  };
+function tennis()
+ {
+     pointA = 0;
+     pointB = 0;
+ this.playAgetscore = function()
+   {
+       pointA += 1;
+       return this.echo();
+   }
+ this.playBgetscore = function()
+   {
+       pointB += 1;
+       return this.echo();
+   }
+ this.echo = function()
+   {
+         var scoreA = '';
+         var scoreB = '';
+         // A
+      if(pointA === 0)
+        {
+           scoreA = 'Love';
+        }
+    else  if(pointA === 1)
+        {
+           scoreA = 'fifteen';
+        }
+    else  if(pointA === 2)
+        {
+           scoreA = 'thirty';
+        }
+    else  if(pointA === 3)
+          {
+             scoreA = 'forty';
+          }
+    else if(pointA === 4)
+        {
+           scoreA = 'A won the game';
+           pointA = 0;
+           return scoreA;
+        }
+      // B
+      if(pointB === 0)
+        {
+           scoreB = 'Love';
+        }
+      else if(pointB === 1)
+        {
+           scoreB = 'fifteen';
+        }
+      else if(pointB === 2)
+        {
+           scoreB = 'thirty';
+        }
+      else if(pointB === 3)
+          {
+             scoreB = 'forty';
+          }
+      else if(pointB === 4)
+      {
+         scoreB = 'B won the game';
+          pointB = 0;
+         return scoreB;
+      }
+  return scoreA+'-'+scoreB;
+  }
 }
 
-  describe('Tennis game', function(){
-	  it('should  be  "Love = Love" when starting the game',function()
-	  {
-       var point = new tennispoint();
-	      point.playAGetscore = 0;
-	      point.playBGetscore = 0;
-	      expect(point.echo()).toEqual('Love-Love');
-	  });
-    it('should  be  "fifteen = Love" when A=15 and B=0',function()
-    {
-       var point = new tennispoint();
-        point.playAGetscore = 15;
-        point.playBGetscore = 0;
-        expect(point.echo()).toEqual('fifteen-Love');
-    });
-    it('should  be  "thirty = Love" when A=30 and B=0',function()
-    {
-      var point = new tennispoint();
-      point.playAGetscore = 30;
-      point.playBGetscore = 0;
-      expect(point.echo()).toEqual('thirty-Love');
-    });
-    it('should  be  "Forty = Love" when A=40 and B=0',function()
-    {
-       var point = new tennispoint();
-        point.playAGetscore = 40;
-        point.playBGetscore = 0;
-        expect(point.echo()).toEqual('Forty-Love');
-    });
+describe('Tennis game', function()
+{
+   var point = new tennis();
+  it('should  be  "Love-Love" when starting the game',function()
+  {
+   expect(point.echo()).toEqual('Love-Love');
+  });
+  //caseA get point
+  it('should  be  "fifteen-Love" when A=15 and B=0',function()
+  {
+   expect(point.playAgetscore()).toEqual('fifteen-Love');
+  });
+  it('should  be  "thirty-Love" when A=30 and B=0',function()
+  {
+   expect(point.playAgetscore()).toEqual('thirty-Love');
+  });
+  it('should  be  "forty-Love" when A=40 and B=0',function()
+  {
+   expect(point.playAgetscore()).toEqual('forty-Love');
+  });
+  it('should  be  "A won the game" when A win',function()
+  {
+   expect(point.playAgetscore()).toEqual('A won the game');
+  });
 
 
-});
+ });
